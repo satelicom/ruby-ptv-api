c1704c8dbf1aa3c1c14e74721ec120e7f292ea60
Michele <michele@leanbit.it>
2016-05-30 10:57:38 +0200

    complete calculate route request

---
+++ spec/operation/x_route/calculate_route_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe SatelicomPtv::Operation::XRoute::CalcutateRoute do
+  let(:details) do 
+    SatelicomPtv::Model::XRoute::ResultListOptions.new(
+      "binaryPathDesc" => false,
+      "boundingRectanglesC" =>  5,
+      "boundingRectanglesOffset" =>  300,
+      "brunnelManoeuvres" =>  false,
+      "detailLevel" =>  :STANDARD,
+      "dynamicInfo" =>  false,
+      "manoeuvreAttributes" =>  false,
+      "manoeuvreGroupRatio" =>  1,
+      "manoeuvreGroups" => true,
+      "manoeuvres" => true,
+      "nodes" => false,
+      "polygon" => true,
+      "segmentAttributes" => false,
+      "segments" => true,
+      "texts" => true,
+      "totalRectangle" =>  true,
+      "urbanManoeuvres" =>  false
+    )
+  end
+  let(:plain_points) { [create(:leanbit_plain_point), create(:satelicom_plain_point)] }
+  let(:waypoints){ XTourTest.create_waypoint_descs(plain_points) }
+  it 'call' do
+    VCR.use_cassette("calculate_route") do
+      function = SatelicomPtv::Operation::XRoute::CalcutateRoute.new(
+        waypoints: waypoints, 
+        options: [], 
+        exceptionPaths: [], 
+        details: details
+      )
+      route = function.call
+      expect(route.info.distance).to be_between(35000, 37000) # more or less 36km between companies
+    end
+  end
+end
--- spec/support/xtour_test.rb
+++ spec/support/xtour_test.rb
@@ -39,5 +39,25 @@ module XTourTest
       end
       orders
     end
+
+    def create_waypoint_desc(plain_point)
+      SatelicomPtv::Model::XRoute::WaypointDesc.new(
+        'coords' => [
+          FactoryGirl.create(:point, 'point' => plain_point)
+        ],
+        "linkType" => :NEXT_SEGMENT
+      )
+    end
+
+    def create_waypoint_descs(plain_points)
+      orders = []
+      id = 0
+      plain_points.each do |plain_point|
+        id += 1
+        orders << create_waypoint_desc(plain_point)
+      end
+      orders
+    end
+
   end
 end
